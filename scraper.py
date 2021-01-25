459c02a060f65e7c275342be2a55718b49a8b3a7
Joseph Keable <keablejoseph@gmail.com>
2021-01-25 16:07:23 +0000

    adds basic class name and methods and import statement to main.py

---
+++ scraper.py
@@ -0,0 +1,20 @@
+class JobScrape():
+
+    def __init__():
+        pass
+
+    def _format_monster():
+        pass
+    
+    def _get_description():
+        pass
+    
+    def _scrape_site():
+        pass
+    
+    def get_jobs():
+        pass
+    
+
+
+  
